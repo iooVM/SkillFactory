89aecf88dc09b18edbeda82967972e86558b20e6
Ширшаков Михаил Анатольевич <m.shirshakov@VP.local>
2021-07-08 17:09:55 +0300

    Modul 1.9

---
+++ Modul_C1.9/rectangle_2.py
@@ -0,0 +1,9 @@
+from rectangle import Rectangle
+
+#далее создаем два прямоугольника
+
+rect_1 = Rectangle(3,4)
+rect_2 = Rectangle(12,5)
+#вывод площади наших двух прямоугольников
+print(rect_1.get_area())
+print(rect_2.get_area())
