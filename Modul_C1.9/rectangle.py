45757a94495793778c14c93b6899cafc92273b0a
Ширшаков Михаил Анатольевич <m.shirshakov@VP.local>
2021-07-09 16:58:51 +0300

    version 4

--- Modul_C1.9/rectangle.py
+++ Modul_C1.9/rectangle.py
@@ -1,12 +1,4 @@
 class Rectangle:
-    def __init__(self,a,b):
-        self.a = a
-       	self.b = b
-    def get_area(self):
-        return self.a * self.b
-
-
-class Rectangle:
     def __init__(self, a, b):
         self.a = a
         self.b = b
