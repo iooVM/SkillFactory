91a02802cd56039aac7b887acfecd2d168cec2f0
Ширшаков Михаил Анатольевич <m.shirshakov@VP.local>
2021-07-13 09:19:46 +0300

    Modul_C23

---
+++ Modul_C1.9/points.py
@@ -0,0 +1,28 @@
+class Dot:
+    def __init__(self,x,y):
+       self.x=x
+       self.y=y
+
+
+    def __eq__(self,other):
+        return self.x==other.x and self.y==other.y
+
+
+    def __str__(self):
+        return f'Dot: {self.x,self.y}'
+
+
+class Circle
+    def __init__(self,r):
+        self.r = r
+    def radius(self):
+        re
+
+
+
+p1=Dot(1,2)
+p2=Dot(1,2)
+print(p1==p2)
+print(str(p1))
+print(p2)
+
