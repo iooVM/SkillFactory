03f5c79751d524baf6f00b4516d030cce4612834
Ширшаков Михаил Анатольевич <m.shirshakov@VP.local>
2021-07-14 08:17:19 +0300

    20210714

--- Modul_C2_final_task/Naval_Battle.py
+++ Modul_C2_final_task/Naval_Battle.py
@@ -29,6 +29,7 @@
 #  ['О'],['О'],['О'],['О'],['О'],['О'],
 #  ['О'],['О'],['О'],['О'],['О'],['О']]
 
+# Нарисуем ручками поле
 Pole = [[ 'О','О','О','О','О','О'],
  ['О','О','О','О','О','О'],
  ['О','О','О','О','О','О'],
@@ -37,7 +38,7 @@ Pole = [[ 'О','О','О','О','О','О'],
  ['О','О','О','О','О','О']]
 
 
-
+#Класс точек на поле
 class Dot:
     def __init__(self, x, y):
          self.x = x
@@ -56,46 +57,73 @@ class Dot:
     #     if  ((y > 0) and isinstance(y, int)):
     #         self.y = y
 
-
+    # Для сравнение точек
     def __eq__(self,other):
         return self.x==other.x and self.y==other.y
     def __str__(self):
-        return f'Dot: {self.x,self.y}'
+        return f' X:{self.x}, Y:{self.y}'
 
 # Класс корабль
 class Ship:
-    def __init__(self, size,dotship,health,direction = True,):
+    def __init__(self, size,dotship,direction = True):
         #Длина корабля
         self.size = size
+#        print(size)
         #Координаты носа корабль
         self.dotship = dotship
         #Направление корабля, по умолчанию горизонталь, т.е. True
         self.direction = direction
         #Жизни
-        self.health = health
-    #Возвращает список всех точек корабля
+        self.health = size
+        ## Массив координат корабля, к примеру Ship.dots() = [Dot,Dot,Dot]
+        self.shipdots = []
+
+
+        #Возвращает список всех точек корабля
     def dots(self):
-        # Массив координат корабля, к примеру Ship.dots() = [Dot,Dot,Dot]
-        shipdots = []
-        return shipdots
+
+        if self.size > 1:
+            pass
+        else:
+#            print(self.dotship)
+            self.shipdots.append(self.dotship)
+#            print(self.shipdots)
+            return self.shipdots
+
+        # if size:
+        #     if direction dotship
+        # return shipdots
         #  По умолчанию по горизонтали
 
+        pass
+
+
+    def __str__(self):
+        return f'Ship: Размер: {self.size},Координаты носа:{self.dotship}, Направление корабля {self.direction }, жизни: {self.health}'
+
 def print_pole(pole):
-    print(' ', *range(len(pole)))
+    print('    ', *range(1,len(pole)+1))
     for i in range(len(pole)):
-        print(i+1, *pole[i])
+        print('',i+1,'|', *pole[i])
 
 
 
 if __name__ == '__main__':
     p1=Dot(1,2)
     p2=Dot(1,2)
-    print(p1==p2)
-    print(str(p1))
-    print(p2)
-    print('test')
+    # print(p1==p2)
+    # print(str(p1))
+    # print(p2)
     print_pole(Pole)
-
+    # print(p1.x)
+    # print(p1.y)
+    Ship1=Ship(1,p1,True)
+    #print(type(Ship1.dots()))
+    print(Ship1)
+    Ship1Dots = Ship1.dots()
+#    print(Ship1Dots)
+    # for i in Ship1Dots:
+    #     print('1',i)
 
 # print(Pole)
 # print('test')
