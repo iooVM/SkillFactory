aa60eb29e3a17ab684127c3b993f7665847cf9c8
Shirshakov Mikhail <mihail@shirshakov.ru>
2021-08-01 18:19:32 +0300

    change1

--- Modul_C2_final_task/Naval_Battle.py
+++ Modul_C2_final_task/Naval_Battle.py
@@ -1,24 +1,24 @@
-
 # Pole = [ ['О'],['О'],['О'],['О'],['О'],['О'],
 #  ['О'],['О'],['О'],['О'],['О'],['О'],
 #  ['О'],['О'],['О'],['О'],['О'],['О'],
 #  ['О'],['О'],['О'],['О'],['О'],['О'],
 #  ['О'],['О'],['О'],['О'],['О'],['О'],
 #  ['О'],['О'],['О'],['О'],['О'],['О']]
-#Пустое поле
-Pole = [[ 'О','О','О','О','О','О'],
- ['О','О','О','О','О','О'],
- ['О','О','О','О','О','О'],
- ['О','О','О','О','О','О'],
- ['О','О','О','О','О','О'],
- ['О','О','О','О','О','О']]
+# Пустое поле
+# Pole = [[ 'О','О','О','О','О','О'],
+#  ['О','О','О','О','О','О'],
+#  ['О','О','О','О','О','О'],
+#  ['О','О','О','О','О','О'],
+#  ['О','О','О','О','О','О'],
+#  ['О','О','О','О','О','О']]
 
 
-#Класс точка
+# Класс точка
 class Dot:
     def __init__(self, x, y):
-         self.x = x
-         self.y = y
+        self.x = x
+        self.y = y
+
     # # Геттер и сеттер для Х
     # def get_x(self,x):
     #     return self.x
@@ -33,48 +33,58 @@ class Dot:
     #     if  ((y > 0) and isinstance(y, int)):
     #         self.y = y
 
-    #Переопределим параметр равенства точек
-    def __eq__(self,other):
-            return self.x==other.x and self.y==other.y
-    #Переопределим вывод точки , для корректного отображение содержимого, а не адресов на содержимое, используем __repr__
+    # Переопределим параметр равенства точек
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    # Переопределим вывод точки , для корректного отображение содержимого, а не адресов на содержимое, используем __repr__
     def __repr__(self):
-        return f'Dot: {self.x,self.y}'
+        return f'Dot{self.x, self.y}'
 
 
-#Класс исключений
+# Класс исключений
 class BoardException(Exception):
     pass
-#Исключение при попадании сне доски
+
+
+# Исключение при попадании сне доски
 class BoardOutException(BoardException):
     def __str__(self):
         return 'внедоски '
-#Исключение при повторном выстреле
+
+
+# Исключение при повторном выстреле
 class BoardUsedException(BoardException):
     def __str__(self):
         return "повторный выстрел"
-#Исключение при невозможности размещения корабля
+
+
+# Исключение при невозможности размещения корабля
 class BoardWrongShipException(BoardException):
     pass
 
+
 # Класс корабль
 class Ship:
-    def __init__(self, dotship,size,direction = True,):
+    def __init__(self, dotship, size, direction=True, ):
 
-        #Координаты носа корабль
+        # Координаты носа корабль
         self.dotship = dotship
         # Длина корабля
         self.size = size
-        #Направление корабля, по умолчанию горизонталь, т.е. True
+        # Направление корабля, по умолчанию горизонталь, т.е. True
         self.direction = direction
-        #Жизни
+        # Жизни
         self.health = size
 
+    # Координаты точек корабля
+    #    @property
     def dots(self):
         # Массив координат корабля, к примеру Ship.dots() = [Dot,Dot,Dot]
         shipdots = []
         # Возвращает список всех точек корабля
         for i in range(self.size):
-            #Буферные параметры точек, для записи в список точек корабля
+            # Буферные параметры точек, для записи в список точек корабля
             dot_x = self.dotship.x
             dot_y = self.dotship.y
 
@@ -83,31 +93,49 @@ class Ship:
                 dot_x += i
             else:
                 dot_y += i
-            #Добавление точки в список точек корабля
-            shipdots.append(Dot(dot_x,dot_y))
+            # Добавление точки в список точек корабля
+            shipdots.append(Dot(dot_x, dot_y))
         return shipdots
-        #  По умолчанию по горизонтали
 
-def print_pole(pole):
-    print(' ', *range(len(pole)))
-    for i in range(len(pole)):
-        print(i+1, *pole[i])
+    # Проверка на попадание в корабль
+    def hited(self, hit):
+        return hit in self.dots()
 
+
+# def print_pole(pole):
+#     print(' ', *range(len(pole)))
+#     for i in range(len(pole)):
+#         print(i+1, *pole[i])
+
+# Класс доска
+class Board:
+    def __int__(self, hiden=False, size=6):
+        # Скрытая доска, по умолчанию нет
+        self.hiden = hiden
+        # Размер доски, по умолчанию 6
+        self.size = size
+        # Создадим пустое поле
+        self.pole = [['0'] * size for i in range(size)]
+
+    def __str__(self):
+        return f'Привет{self.size}'
 
 
 if __name__ == '__main__':
-    p1=Dot(1,2)
-    p2=Dot(1,2)
-    print(p1==p2)
-    print(str(p1))
-    print(p2)
-    print('test')
-    print_pole(Pole)
-    #Тестовый кораблик
-    ship_test = Ship(p1, 3, False)
-    print(ship_test.dots())
+    # p1=Dot(1,2)
+    # p2=Dot(1,2)
+    # print(p1==p2)
+    # print(str(p1))
+    # print(p2)
+    # # print_pole(Pole)
+    # #Тестовый кораблик
+    # ship_test = Ship(p1, 3, False)
+    # print(ship_test.dots())
+    # print(ship_test.hited(Dot(0, 3)))
+    b = Board()
+    print(b)
+
 
 
 # print(Pole)
 # print('test')
-
