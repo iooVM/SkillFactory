2c208ca60c61fcd34b4a7ad5897cda462645c030
Ширшаков Михаил Анатольевич <m.shirshakov@VP.local>
2021-08-13 12:23:49 +0300

    20210813

--- Modul_C5_final_task/main.py
+++ Modul_C5_final_task/main.py
@@ -1,18 +1,6 @@
-x# This is a sample Python script.
+import telebot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+TOKEN = "Токен полученный при регистрации"
 
+bot = telebot.TeleBot(TOKEN)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-яыва
-zasxc\
